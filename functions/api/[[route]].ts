ec729896e9a83a5515ecd1d9c18b7f90ab01b7c5
Yousef <yousefsameh24@gmail.com>
2024-03-15 19:51:11 +0200

    Add new route for environment variables

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,10 +1,12 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/cloudflare-pages'
 import test from './routes/test'
+import Env from './routes/translate'
 
 const app = new Hono()
   .basePath('/api')
   .route('/todo', test)
+  .route('/env', Env)
 
 export type AppType = typeof app
 
