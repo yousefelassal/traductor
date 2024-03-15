e07bd81d425482bd08bcc589abace491ae011117
Yousef <yousefsameh24@gmail.com>
2024-03-15 23:51:18 +0200

    Refactor API routes and integrate database***

--- functions/api/routes/test.ts
+++ functions/api/routes/test.ts
@@ -2,25 +2,23 @@ import { Hono } from 'hono'
 import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
 import { env } from 'hono/adapter'
+import { db } from '../db'
 
 const schema = z.object({
-    id: z.string(),
-    title: z.string(),
-  })
-  
-type Todo = z.infer<typeof schema>
-  
-const todos: Todo[] = []
+  id: z.string(),
+  title: z.string(),
+})
 
 const test = new Hono()
-  .post('/', zValidator('form', schema), (c) => {
+  .post('/', zValidator('form', schema), async (c) => {
     const todo = c.req.valid('form')
-    todos.push(todo)
+    await db.test.create({ data: todo })
     return c.json({
-      message: 'created!',
+        todo
     })
   })
-  .get('/', (c) => {
+  .get('/', async (c) => {
+    const todos = await db.test.findMany()
     return c.json({
         todos
     })
