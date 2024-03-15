7cf8d259b72a0f76b93389d3cd1f552f0f6720b1
Yousef <yousefsameh24@gmail.com>
2024-03-15 22:24:11 +0200

    Fix import statement in [[route]].ts

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/cloudflare-pages'
 import test from './routes/test'
-import Env from './routes/translate'
+import { Env } from './routes/test'
 
 const app = new Hono()
   .basePath('/api')
