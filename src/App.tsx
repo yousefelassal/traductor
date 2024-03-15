71102e2d43a3f8c3e5ba92af7f3c315fd924c6b8
Yousef <yousefsameh24@gmail.com>
2024-03-15 13:15:19 +0200

    Add React Query and Todo functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,68 @@
+import { useQuery, useMutation, QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AppType } from '../functions/api/[[route]]'
+import { hc, InferResponseType, InferRequestType } from 'hono/client'
+
+const queryClient = new QueryClient()
+const client = hc<AppType>('/api')
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <QueryClientProvider client={queryClient}>
+      <Todos />
+    </QueryClientProvider>
+  )
+}
+
+const Todos = () => {
+  const query = useQuery({
+    queryKey: ['todos'],
+    queryFn: async () => {
+      const res = await client.api.todo.$get()
+      return await res.json()
+    },
+  })
+
+  const $post = client.api.todo.$post
+
+  const mutation = useMutation<
+    InferResponseType<typeof $post>,
+    Error,
+    InferRequestType<typeof $post>['form']
+  >(
+    async (todo) => {
+      const res = await $post({
+        form: todo,
+      })
+      return await res.json()
+    },
+    {
+      onSuccess: async () => {
+        queryClient.invalidateQueries({ queryKey: ['todos'] })
+      },
+      onError: (error:any) => { // eslint-disable-line
+        console.log(error)
+      },
+    }
+  )
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          mutation.mutate({
+            id: Date.now().toString(),
+            title: 'Write code',
+          })
+        }}
+      >
+        Add Todo
+      </button>
+
+      <ul>
+        {query.data?.todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
