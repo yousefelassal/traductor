daeaf5894d6ca1efaa3da7a15edbaf369c0cccaa
Yousef <yousefsameh24@gmail.com>
2024-03-15 19:13:37 +0200

    Refactor API routes and add new '/todo' route

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -1,33 +1,11 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/cloudflare-pages'
-import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator'
+import test from './routes/test'
 
-const app = new Hono().basePath('/api')
+const app = new Hono()
+  .basePath('/api')
+  .route('/todo', test)
 
-const schema = z.object({
-  id: z.string(),
-  title: z.string(),
-})
-
-type Todo = z.infer<typeof schema>
-
-const todos: Todo[] = []
-
-const route = app
-  .post('/todo', zValidator('form', schema), (c) => {
-    const todo = c.req.valid('form')
-    todos.push(todo)
-    return c.json({
-      message: 'created!',
-    })
-  })
-  .get((c) => {
-    return c.json({
-      todos,
-    })
-  })
-
-export type AppType = typeof route
+export type AppType = typeof app
 
 export const onRequest = handle(app)
