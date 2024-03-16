3e7a48216d26895ef174c7309ba55307cae5433f
Yousef <yousefsameh24@gmail.com>
2024-03-16 18:11:05 +0200

    Add translation API endpoint

--- functions/api/routes/translate.ts
+++ functions/api/routes/translate.ts
@@ -1,10 +1,35 @@
-// import { Hono } from 'hono'
-// import { z } from 'zod'
-// import { zValidator } from '@hono/zod-validator'
+import { Hono } from 'hono'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid'
+import { env } from 'hono/adapter'
 
-// const schema = z.object({
-//     id: z.string(),
-//     title: z.string(),
-// })
-  
-// const translator = new Hono()
+const schema = z.object({
+    text: z.string(),
+})
+
+const endpoint = "https://api.cognitive.microsofttranslator.com"
+
+const translator = new Hono()
+.post('/', zValidator('form', schema), async (c) => {
+    const { AZURE_TRANSLATOR_KEY, AZURE_LOCATION } = env(c)
+    const text = c.req.valid('form')
+    const key = AZURE_TRANSLATOR_KEY
+    const region = AZURE_LOCATION
+    const url = `${endpoint}/translate?api-version=3.0&to=es`
+    const body = [{ text }]
+    const { data } = await axios.post(url, body, {
+        headers: {
+            'Ocp-Apim-Subscription-Key': key as string,
+            'Ocp-Apim-Subscription-Region': region as string,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': uuidv4().toString()
+        }
+    })
+    return c.json({
+        translation: data[0].translations[0].text
+    })
+})
+
+export default translator
