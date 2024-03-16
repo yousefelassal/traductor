7405d614aad2f38a1c203e0e7c8f9a42664d98b4
Yousef <yousefsameh24@gmail.com>
2024-03-16 12:30:28 +0200

    Update PrismaClient configuration

--- functions/api/db.ts
+++ functions/api/db.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
-export const db = new PrismaClient().$extends(withAccelerate())
+export const db = new PrismaClient({
+    datasourceUrl: 'prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiMzI1ODNkMzgtNWU0Ni00NTA5LWE2ZjktMTBmZmVhZDZkODU2IiwidGVuYW50X2lkIjoiNzZjOTMxYjExMDU3YmU0ZjE1ZGRjNTdhMjIzMDBkYmE4OWFlMTc5MGNiNzlhMmEyZTRkZGUyNDQ1NGZmMDg4ZSIsImludGVybmFsX3NlY3JldCI6ImIxOTA1Y2ExLTMyMjYtNGExYi04ZTY2LTBkMjJjODM3ZmQ2ZiJ9.ujk7MR_fnzmsZhYVpgJZQKMMru0IFJuJ1JeLJLyUloY'
+}).$extends(withAccelerate())
