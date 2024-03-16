b2857bff6667bd33700633b1360115556e1ce739
Yousef <yousefsameh24@gmail.com>
2024-03-16 18:10:51 +0200

    Add translate route to API

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -2,11 +2,13 @@ import { Hono } from 'hono'
 import { handle } from 'hono/cloudflare-pages'
 import test from './routes/test'
 import { Env } from './routes/test'
+import translator from './routes/translate'
 
 const app = new Hono()
   .basePath('/api')
   .route('/todo', test)
   .route('/env', Env)
+  .route('/translate', translator)
 
 export type AppType = typeof app
 
