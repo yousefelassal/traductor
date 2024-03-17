763482c68e5af44ebcd098fe57c6fb826198b9b5
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-17 10:57:03 +0000

    Add route for text-to-speech functionality

--- functions/api/[[route]].ts
+++ functions/api/[[route]].ts
@@ -3,12 +3,14 @@ import { handle } from 'hono/cloudflare-pages'
 import test from './routes/test'
 import { Env } from './routes/test'
 import translator from './routes/translate'
+import tts from './routes/tts'
 
 const app = new Hono()
   .basePath('/api')
   .route('/todo', test)
   .route('/env', Env)
   .route('/translate', translator)
+  .route('/tts', tts)
 
 export type AppType = typeof app
 
