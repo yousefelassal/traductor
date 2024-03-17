3a01db582e875ff9af74aa7b135722e2d2ad0025
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-17 12:58:35 +0000

    Add language support for Azure TTS

--- functions/api/routes/tts.ts
+++ functions/api/routes/tts.ts
@@ -12,10 +12,22 @@ const schema = z.object({
 const tts = new Hono()
 .post('/', zValidator('form', schema), async (c) => {
     const { AZURE_TTS_KEY, AZURE_TTS_LOCATION } = env(c)
-    const { text } = c.req.valid('form')
+    const { text, lang } = c.req.valid('form')
     const key = AZURE_TTS_KEY
     const region = AZURE_TTS_LOCATION
+    const spokenLang = lang === 'en' ? 'en-GB'
+    : lang === 'es' ? 'es-ES'
+    : lang === 'fr' ? 'fr-FR'
+    : lang === 'de' ? 'de-DE'
+    : lang === 'it' ? 'it-IT'
+    : lang === 'ja' ? 'ja-JP'
+    : lang === 'ko' ? 'ko-KR'
+    : lang === 'pt' ? 'pt-PT'
+    : lang === 'ru' ? 'ru-RU'
+    : lang === 'ar' ? 'ar-EG'
+    : 'en-GB'
     const speechConfig = sdk.SpeechConfig.fromSubscription(key as string, region as string);
+    speechConfig.speechSynthesisLanguage = spokenLang
     const synthesizer = new sdk.SpeechSynthesizer(speechConfig);
     const audioStram = await new Promise((resolve, reject) => {
         synthesizer.speakTextAsync(text, (result) => {
