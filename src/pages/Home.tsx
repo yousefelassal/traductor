f1ad4efffc512ad1a294c234fb5497a4da87774b
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-17 14:28:27 +0000

    Refactor Home component and add Sidebar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,62 +1,15 @@
-import { useTranslationStore } from '../../stores/translation'
-import { useAutoAnimate } from '@formkit/auto-animate/react'
 import TypingForm from '../components/TypingForm'
+import { useTranslationStore } from '../../stores/translation'
+import Sidebar from '../components/Sidebar'
 
 const Home = () => {
-  const [animationParent] = useAutoAnimate()
-
-  const {
-    allTranslations,
-    currentTranslation,
-    setCurrentTranslation
-  } = useTranslationStore()
-
-  const convertLangCode = (code: string) => {
-    const lang = new Intl.DisplayNames(['en'], { type: 'language' });
-    return lang.of(code)
-  }
+  const { currentTranslation } = useTranslationStore()
 
   return (
-    <div className="relative min-h-screen container mx-auto py-12 flex flex-col gap-2 items-center">
+    <div className="relative min-h-screen w-screen flex">
+      <Sidebar />
       <h1 className="text-3xl font-bold">{currentTranslation?.translation} </h1>
       <TypingForm />
-      <div
-        ref={animationParent}
-        className="flex flex-col gap-1 mb-24"
-      >
-        {allTranslations.map((translation) => (
-          <div key={translation.id} className="rounded-md border shadow-md">
-            <p>from: {convertLangCode(translation.from_lang)}</p>
-            <p>to: {convertLangCode(translation.to_lang)}</p>
-            <p>{translation.translation}</p>
-            {currentTranslation?.id === translation.id ? (
-              <button
-                onClick={() => {
-                  setCurrentTranslation(null)
-                  translation.audio.pause()
-                }}
-                className="rounded-md border shadow px-2 py-1 bg-black/10 hover:bg-black/20 transition-colors duration-200"
-              >
-                Stop
-              </button>
-            )
-            : 
-            (
-              <button
-                onClick={() => {
-                  setCurrentTranslation(translation)
-                  translation.audio.currentTime = 0
-                  translation.audio.onended = () => setCurrentTranslation(null)
-                  translation.audio.play()
-                }}
-                className="rounded-md border shadow px-2 py-1 bg-black/10 hover:bg-black/20 transition-colors duration-200"
-              >
-                Play
-              </button>
-            )}
-          </div>
-        ))}
-      </div>
     </div>
   )
 }
