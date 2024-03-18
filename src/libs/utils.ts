5df63bc48e50e1292a19422a6c1113881d666b76
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-18 05:18:37 +0000

    Add cn utility function for merging class names

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -1,6 +1,12 @@
 import { AppType } from '../../functions/api/[[route]]'
 import { hc } from 'hono/client'
 import { QueryClient } from '@tanstack/react-query'
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 export const queryClient = new QueryClient()
 export const client = hc<AppType>('/')
