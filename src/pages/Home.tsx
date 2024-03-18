443eaf6397e3c5cc25bb8d8b9e19e65c513cd4df
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-18 14:47:41 +0000

    Add language flags and convert language codes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import TypingForm from '../components/TypingForm'
 import { useTranslationStore } from '../../stores/translation'
 import Sidebar from '../components/Sidebar'
 import { BackgroundGradientAnimation } from '../components/ui/background-gradient'
+import { convertLangToFlag, convertLangCode } from '../libs/utils'
+import ReactCountryFlag from 'react-country-flag'
 
 const Home = () => {
   const { currentTranslation, loading } = useTranslationStore()
@@ -17,18 +19,40 @@ const Home = () => {
                   Translating...
               </p>
             : 
-              <div className="flex flex-col gap-2">
+              <div className="flex flex-col gap-4 w-full h-full">
                 <div className="flex justify-between text-base">
+                  <div className="flex flex-col items-center">
+                    <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
+                      {convertLangCode(currentTranslation.from_lang)}
+                    </p>
+                    <ReactCountryFlag
+                      countryCode={convertLangToFlag(currentTranslation.from_lang)}
+                      className="text-4xl"
+                      svg 
+                    />
+                    <p className="bg-clip-text font-thin text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
+                      From
+                    </p>
+                  </div>
+                  <div className="flex flex-col items-center">
+                    <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
+                      {convertLangCode(currentTranslation.to_lang)}
+                    </p>
+                    <ReactCountryFlag
+                      countryCode={convertLangToFlag(currentTranslation.to_lang)}
+                      className="text-4xl"
+                      svg 
+                    />
+                    <p className="bg-clip-text font-thin text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
+                      To
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-1 items-center justify-center">
                   <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
-                    {currentTranslation.from_lang}
-                  </p>
-                  <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
-                    {currentTranslation.to_lang}
+                    {currentTranslation.translation}
                   </p>
                 </div>
-                <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
-                  {currentTranslation.translation}
-                </p>
               </div>
           )
           :
