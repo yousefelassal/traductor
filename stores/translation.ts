c4a0b8856cd09bff4df892bdc63615bafd6d4a26
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-18 06:13:53 +0000

    Add isAudioPlaying to translation store

--- stores/translation.ts
+++ stores/translation.ts
@@ -12,17 +12,21 @@ type translationStore = {
     allTranslations: translation[],
     currentTranslation: translation | null,
     loading: boolean
+    isAudioPlaying: boolean
     addTranslation: (translation: translation) => void,
     setCurrentTranslation: (translation: translation | null) => void
     setLoading: (loading: boolean) => void
+    setIsAudioPlaying: (isAudioPlaying: boolean) => void
 }
 
 export const useTranslationStore = create<translationStore>((set) => ({
     allTranslations: [],
     currentTranslation: null,
     loading: false,
+    isAudioPlaying: false,
     addTranslation: (translation) => set((state) => ({ allTranslations: [...state.allTranslations, translation] })),
     setCurrentTranslation: (translation) => set(() => ({ currentTranslation: translation })),
-    setLoading: (loading) => set(() => ({ loading }))
+    setLoading: (loading) => set(() => ({ loading })),
+    setIsAudioPlaying: (isAudioPlaying) => set(() => ({ isAudioPlaying }))
 
 }))
