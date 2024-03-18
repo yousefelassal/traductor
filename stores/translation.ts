51e7cf7dd8cd6a0136c7a05503baddabb638ae62
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-18 06:30:55 +0000

    Add audio type and currentAudio state to translation store

--- stores/translation.ts
+++ stores/translation.ts
@@ -1,32 +1,38 @@
 import { create } from 'zustand'
 
+type audio = {
+    id: string
+    audio: HTMLAudioElement
+}
+
 type translation = {
     id: string
     from_lang: string
     translation: string
     to_lang: string
-    audio: HTMLAudioElement
+    audio: audio
 }
+
 
 type translationStore = {
     allTranslations: translation[],
     currentTranslation: translation | null,
+    currentAudio: audio | null,
     loading: boolean
-    isAudioPlaying: boolean
     addTranslation: (translation: translation) => void,
     setCurrentTranslation: (translation: translation | null) => void
+    setCurrentAudio: (audio: audio | null) => void
     setLoading: (loading: boolean) => void
-    setIsAudioPlaying: (isAudioPlaying: boolean) => void
 }
 
 export const useTranslationStore = create<translationStore>((set) => ({
     allTranslations: [],
     currentTranslation: null,
+    currentAudio: null,
     loading: false,
-    isAudioPlaying: false,
     addTranslation: (translation) => set((state) => ({ allTranslations: [...state.allTranslations, translation] })),
     setCurrentTranslation: (translation) => set(() => ({ currentTranslation: translation })),
+    setCurrentAudio: (audio) => set(() => ({ currentAudio: audio })),
     setLoading: (loading) => set(() => ({ loading })),
-    setIsAudioPlaying: (isAudioPlaying) => set(() => ({ isAudioPlaying }))
 
 }))
