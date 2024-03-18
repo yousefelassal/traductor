e70577b05e61cf5f6bf68668d27968fb150c7ddd
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-18 14:25:59 +0000

    Add language flags to sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,6 +3,7 @@ import { useAutoAnimate } from '@formkit/auto-animate/react'
 import { useTranslationStore } from '../../stores/translation'
 import { PauseIcon, PlayIcon } from '@heroicons/react/24/solid'
 import { cn } from '../libs/utils'
+import ReactCountryFlag from 'react-country-flag'
 
 const Sidebar = () => {
   const [animationParent] = useAutoAnimate()
@@ -20,8 +21,35 @@ const Sidebar = () => {
     return lang.of(code)
   }
 
+  const convertLangToFlag = (code: string) => {
+    switch (code) {
+      case 'en':
+        return 'GB'
+      case 'es':
+        return 'ES'
+      case 'fr':
+        return 'FR'
+      case 'it':
+        return 'IT'
+      case 'pt':
+        return 'PT'
+      case 'ru':
+        return 'RU'
+      case 'ja':
+        return 'JP'
+      case 'ko':
+        return 'KR'
+      case 'ar':
+        return 'SA'
+      case 'de':
+        return 'DE'
+      default:
+        return ''
+    }
+  }
+
   return (
-    <div className="sticky inset-y-0 left-2 min-w-60 z-40 py-2 hidden md:block">
+    <div className="sticky inset-y-0 left-2 min-w-60 max-w-60 z-40 py-2 hidden md:block">
         <div ref={animationParent} className="flex flex-col py-4 px-2 rounded-lg gap-1 bg-gradient-to-b from-slate-300/30 via-gray-400/30 to-slate-600/30 overflow-y-auto h-[calc(100vh-16px)] backdrop-blur-md">
         {allTranslations.length === 0 && (
           <p className="text-center text-gray-400">No translations yet</p>
@@ -29,14 +57,20 @@ const Sidebar = () => {
         {allTranslations.map((translation) => (
           <button
             key={translation.id}
-            className={cn("flex flex-col rounded-xl transition-all px-2 py-1 text-white", 
+            className={cn("flex flex-col gap-1 rounded-xl transition-all px-2 py-1 text-white", 
               currentTranslation?.id === translation.id ? "bg-gradient-to-tr from-purple-400/70 via-violet-400/70 to-purple-300 shadow-md" : "hover:bg-purple-300/60"
             )}
             onClick={() => setCurrentTranslation(translation)}
           >
             <div className="flex justify-between w-full">
-              <p>from: {convertLangCode(translation.from_lang)}</p>
-              <p>to: {convertLangCode(translation.to_lang)}</p>
+              <div className="flex flex-col items-center">
+                <ReactCountryFlag countryCode={convertLangToFlag(translation.from_lang)} svg />
+                <p className="font-thin text-xs">{convertLangCode(translation.from_lang)}</p>
+              </div>
+              <div className="flex flex-col items-center">
+                <ReactCountryFlag countryCode={convertLangToFlag(translation.to_lang)} svg />
+                <p className="font-thin text-xs">{convertLangCode(translation.to_lang)}</p>
+              </div>
             </div>
             <div className="flex justify-between w-full">
             <p className="truncate">{translation.translation}</p>
