8ca8f4960a81049228f43bf98c77db2e6bc0f342
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-18 08:57:41 +0200

    Display loading state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
         <main className="py-20 flex items-center justify-center w-full font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl">
           {currentTranslation ? (
             <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
-              {currentTranslation.translation}
+              {loading ? 'Translating...' : currentTranslation.translation}
             </p>
           )
           :
