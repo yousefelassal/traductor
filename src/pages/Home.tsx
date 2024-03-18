88bfc1164d4a153c7af4020961b6ec59b5cc5900
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-18 14:34:56 +0000

    Refactor translation display in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,11 +10,26 @@ const Home = () => {
     <BackgroundGradientAnimation>
       <div className="relative min-h-screen w-screen flex">
         <Sidebar />
-        <main className="py-20 flex items-center justify-center w-full font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl">
+        <main className="py-20 flex text-white items-center justify-center w-full font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl">
           {currentTranslation ? (
-            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
-              {loading ? 'Translating...' : currentTranslation.translation}
-            </p>
+            loading ? 
+              <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
+                  Translating...
+              </p>
+            : 
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-between text-base">
+                  <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
+                    {currentTranslation.from_lang}
+                  </p>
+                  <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
+                    {currentTranslation.to_lang}
+                  </p>
+                </div>
+                <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
+                  {currentTranslation.translation}
+                </p>
+              </div>
           )
           :
           (
