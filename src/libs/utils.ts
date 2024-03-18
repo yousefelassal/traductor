9d23ec36615a2590a4f93da69ea4ec71d725f0a1
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-18 14:27:14 +0000

    Add language code conversion functions

--- src/libs/utils.ts
+++ src/libs/utils.ts
@@ -8,5 +8,37 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
+export const convertLangCode = (code: string) => {
+  const lang = new Intl.DisplayNames(['en'], { type: 'language' });
+  return lang.of(code)
+}
+
+export const convertLangToFlag = (code: string) => {
+  switch (code) {
+    case 'en':
+      return 'GB'
+    case 'es':
+      return 'ES'
+    case 'fr':
+      return 'FR'
+    case 'it':
+      return 'IT'
+    case 'pt':
+      return 'PT'
+    case 'ru':
+      return 'RU'
+    case 'ja':
+      return 'JP'
+    case 'ko':
+      return 'KR'
+    case 'ar':
+      return 'SA'
+    case 'de':
+      return 'DE'
+    default:
+      return ''
+  }
+}
+
 export const queryClient = new QueryClient()
 export const client = hc<AppType>('/')
