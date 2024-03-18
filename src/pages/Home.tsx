1b9c2f1ea970f2e441fa785c76609b25246eea71
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-18 05:35:07 +0000

    Update Home page styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,15 +10,15 @@ const Home = () => {
     <BackgroundGradientAnimation>
       <div className="relative min-h-screen w-screen flex">
         <Sidebar />
-        <main className="py-20 flex items-center justify-center w-full font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+        <main className="py-20 flex items-center justify-center w-full font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl">
           {currentTranslation ? (
-            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/40">
+            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
               {currentTranslation.translation}
             </p>
           )
           :
           (
-            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/40">
+            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/75">
               Type something to translate
             </p>
           )}
