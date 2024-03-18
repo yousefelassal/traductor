4f236edf8acca724580b8a2ee83fe9a51b8e1614
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-18 05:18:45 +0000

    Add background gradient animation to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,31 @@
 import TypingForm from '../components/TypingForm'
 import { useTranslationStore } from '../../stores/translation'
 import Sidebar from '../components/Sidebar'
+import { BackgroundGradientAnimation } from '../components/ui/background-gradient'
 
 const Home = () => {
   const { currentTranslation } = useTranslationStore()
 
   return (
-    <div className="relative min-h-screen w-screen flex">
-      <Sidebar />
-      <main className="py-20 flex items-center justify-center w-full">
-        {currentTranslation ? (
-          <p className="text-2xl">{currentTranslation.translation}</p>
-        )
-        :
-        (
-          <p className="text-2xl">Type something to translate</p>
-        )}
-      </main>
-      <TypingForm />
-    </div>
+    <BackgroundGradientAnimation>
+      <div className="relative min-h-screen w-screen flex">
+        <Sidebar />
+        <main className="py-20 flex items-center justify-center w-full font-bold px-4 pointer-events-none text-3xl text-center md:text-4xl lg:text-7xl">
+          {currentTranslation ? (
+            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/40">
+              {currentTranslation.translation}
+            </p>
+          )
+          :
+          (
+            <p className="bg-clip-text text-transparent drop-shadow-2xl bg-gradient-to-br from-white/80 to-white/40">
+              Type something to translate
+            </p>
+          )}
+        </main>
+        <TypingForm />
+      </div>
+    </BackgroundGradientAnimation>
   )
 }
 
