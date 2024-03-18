b07157b252073852633fb1ea0f57223f94693bd5
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-18 05:51:37 +0000

    Update Sidebar component styling and layout

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -18,12 +18,15 @@ const Sidebar = () => {
   }
 
   return (
-    <div className="sticky inset-y-0 left-2 min-w-60 z-40 py-2">
+    <div className="sticky inset-y-0 left-2 min-w-60 z-40 py-2 hidden md:block">
         <div ref={animationParent} className="flex flex-col py-4 px-2 rounded-lg gap-1 bg-gradient-to-b from-slate-300/30 via-gray-400/30 to-slate-600/30 overflow-y-auto h-[calc(100vh-16px)] backdrop-blur-md">
         {allTranslations.map((translation) => (
-          <div key={translation.id} className="flex flex-col rounded-md border shadow-md">
-            <p>from: {convertLangCode(translation.from_lang)}</p>
-            <p>to: {convertLangCode(translation.to_lang)}</p>
+          <div key={translation.id} className="flex flex-col rounded-md border bg-gray-300/80 px-2 py-1 shadow-md">
+            <div className="flex justify-between">
+              <p>from: {convertLangCode(translation.from_lang)}</p>
+              <p>to: {convertLangCode(translation.to_lang)}</p>
+            </div>
+            <div className="flex justify-between">
             <p>{translation.translation}</p>
             {currentTranslation?.id === translation.id ? (
               <button
@@ -46,11 +49,12 @@ const Sidebar = () => {
                   translation.audio.play()
                 }}
                 className="rounded-full flex w-fit items-center justify-center border shadow p-1 bg-white/50 hover:bg-white/60 transition-colors duration-200"
-              >
+                >
                 <PlayIcon className="w-6 h-6 fill-white/90" />
               </button>
             )}
           </div>
+        </div>
         ))}
         </div>
     </div>
