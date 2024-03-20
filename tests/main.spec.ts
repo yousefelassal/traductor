0c803d3b44c31db51372c082539d7b28af6b4c2a
Yousef Sameh <yousefsameh24@gmail.com>
2024-03-20 04:59:19 +0000

    Add tests for page title and get started link

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http://127.0.0.1:8788/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Traductor/);
 });
 
 test('get started link', async ({ page }) => {
